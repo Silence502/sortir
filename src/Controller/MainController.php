805d5661af7541c2b89e5a81ee255fc791fbf6c0
Silence502 <36604556+Silence502@users.noreply.github.com>
2021-07-29 12:20:53 +0200

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,6 @@ use App\Data\SearchTripData;
 use App\Form\SearchTripForm;
 use App\Repository\TripRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
