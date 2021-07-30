0cec2bdc79b03439b005e538e601cd50e8623ad1
Silence502 <36604556+Silence502@users.noreply.github.com>
2021-07-30 15:05:36 +0200

    Update TripType.php

--- src/Form/TripType.php
+++ src/Form/TripType.php
@@ -27,19 +27,16 @@ class TripType extends AbstractType
             ])
             ->add('dateStartTime', DateTimeType::class, [
                 'label' => 'Date et heure de début ',
-                'html5' => true,
-                'widget' => 'single_text',
+                'html5' => true
 
             ])
             ->add('duration', TimeType::class, [
                 'label' => 'Durée ',
-                'input'  => 'datetime',
-                'widget' => 'single_text',
+                'input'  => 'datetime'
             ])
             ->add('registrationDeadline', DateTimeType::class, [
                 'label' => 'Date limite d\'inscription ',
-                'html5' => true,
-                'widget' => 'single_text',
+                'html5' => true
 
             ])
             ->add('maxRegistrations', IntegerType::class, [
