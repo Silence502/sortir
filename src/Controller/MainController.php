3d4892ab08b39b9b91c6ebf5b5470eea3fd91912
Silence502 <36604556+Silence502@users.noreply.github.com>
2021-08-04 14:50:35 +0200

    Update admin management
    Can delete user

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Data\SearchTripData;
+use App\Entity\User;
 use App\Form\SearchTripForm;
 use App\Repository\TripRepository;
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,6 +34,11 @@ class MainController extends AbstractController
             $this->getUser()->getUserIdentifier()
         );
 
+
+        if ($user->getIsActive() == 0){
+            return $this->redirectToRoute('desactivated');
+        }
+
         $data = new SearchTripData();
         $searchTripForm = $this->createForm(SearchTripForm::class, $data);
         $searchTripForm->handleRequest($request);
@@ -45,5 +52,15 @@ class MainController extends AbstractController
             'user' => $user,
             'searchForm' => $searchTripForm->createView()
         ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_INACTIVE")
+     * @return Response
+     * @Route("/desactivated", name="desactivated")
+     */
+    public function desactivated(): Response
+    {
+        return $this->render('errors/desactivated.html.twig');
     }
 }
